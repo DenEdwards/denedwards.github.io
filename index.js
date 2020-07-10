8c040d6c60a630fd12b73dd8d16460e867fc8454
DenEdwards <denzelsedwards@gmail.com>
2020-07-10 01:20:28 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//fade reveal
+ScrollReveal({
+    duration: 600,
+    reset: true,
+   easing: 'cubic-bezier(.694,0,.335,1)',
+   scale: 1,
+   viewFactor: 0.3,
+}).reveal('.background');
+
+ScrollReveal({
+  duration: 600,
+  reset: true,
+  easing: 'cubic-bezier(.694,0,.335,1)',
+  scale: 1,
+  viewFactor: 0.3,
+}).reveal('.skills');
+
+ScrollReveal({
+  duration: 600,
+  reset: true,
+  easing: 'cubic-bezier(.694,0,.335,1)',
+  scale: 1,
+  viewFactor: 0.09,
+}).reveal('.projects');
+
+//Get the date
+function year(){
+  const date = new Date();
+  const year = date.getFullYear();
+  $('#year').innerHtml = year;
+}
