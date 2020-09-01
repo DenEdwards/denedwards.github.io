078a46f1df62cbef45fd80b0185d01559f49e8b6
DenEdwards <denzelsedwards@gmail.com>
2020-09-01 01:02:09 -0400

    Removed Project fade

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ ScrollReveal({
   viewFactor: 0.27,
 }).reveal('.skills');
 
-ScrollReveal({
-  duration: 600,
-  reset: true,
-  easing: 'cubic-bezier(.694,0,.335,1)',
-  scale: 1,
-  viewFactor: 0.06,
-}).reveal('.projects');
-
 //Get the date
 function year(){
   const date = new Date();
