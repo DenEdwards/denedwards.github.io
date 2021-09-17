f3a70f581cced129d474b6f7f07e86ae395d5daa
DenEdwards <denzelsedwards@gmail.com>
2021-09-17 19:44:27 -0400

    UI Update

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 [![Contributors][contributors-shield]][contributors-url]
 [![LinkedIn][linkedin-shield]][linkedin-url]
 
-
-
 <!-- PROJECT LOGO -->
 <br />
 <p align="center">
   <a href="https://github.com/DenEdwards/denedwards.github.io">
-    <img src="images/ReadMePic.PNG" alt="Logo" border-radius="7px" width="500" height="auto">
+    <img src="/static/assets/images/ReadMePic.PNG" alt="Logo" border-radius="7px" width="500" height="auto">
   </a>
-
 
   <p align="center">
     Personal Website for Denzel Edwards.
@@ -17,23 +14,20 @@
 </p>
 
 <!-- CONTACT -->
+
 ## Contact
 
-Denzel Edwards -  Denzelsedwards@gmail.com
+Denzel Edwards - Denzelsedwards@gmail.com
 
 Website: [https://denedwards.github.io/](https://denedwards.github.io/)
 
 [contributors-shield]: https://img.shields.io/github/contributors/DenEdwards/denedwards.github.io.svg?style=flat-square
 [contributors-url]: https://github.com/DenEdwards/denedwards.github.io/graphs/contributors
-
 [forks-shield]: https://img.shields.io/github/forks/DenEdwards/denedwards.github.io.svg?style=flat-square
 [forks-url]: https://github.com/DenEdwards/denedwards.github.io/network/members
-
 [stars-shield]: https://img.shields.io/github/stars/DenEdwards/denedwards.github.io.svg?style=flat-square
 [stars-url]: https://github.com/DenEdwards/denedwards.github.io/stargazers
-
 [issues-shield]: https://img.shields.io/github/issues/DenEdwards/denedwards.github.io
 [issues-url]: https://github.com/DenEdwards/denedwards.github.io/issues
-
 [linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
 [linkedin-url]: https://www.linkedin.com/in/denzel-edwards-093927170/
--- index.js
+++ index.js
@@ -1,23 +1,90 @@
+let skills = {
+	Languages: {
+		JavaScript: '#000-#edd718-#000',
+		HTML: '#fff-#e85326-#000',
+		CSS: '#fff-#049ce4-#0474bc',
+		SASS: '#fff-#c96195-#944970',
+		Python: '#000-#fccc39-#3474a7',
+		SQL: '#fff-#de8b00-#025c87',
+		Java: '#fff-#e32e30-#0470b7',
+	},
+	Frameworks_and_Libraries: {
+		JQuery: '#000-#fff-#0865a7',
+		'Node.js': '#fff-#679f63-#333333',
+		'RESTful APIs': '#fff-#000-#0e77ab',
+		React: '#fff-#79d8f7-#000',
+		Redux: '#fff-#7147b5-#000',
+	},
+	Tools: {
+		'Git Collaboration': '#fff-#e74e32-#3c2c01',
+		Devtools: '#fff-#407fef-#000',
+		MongoDB: '#000-#419242-#b8b6b5',
+		Postman: '#fff-#f56933-#b3b3b3',
+		Wireframing: '#fff-#000-#0e77ab',
+		'p5.js': '#fff-#e3245d-#000',
+		Firebase: '#fff-#0396df-#f7c62e',
+	},
+};
+
+(function () {
+	function appendSkill() {}
+	const date = new Date();
+	const year = date.getFullYear();
+	document.getElementById('year').innerText = year;
+
+	for (var skillHeader in skills) {
+		let heading = skillHeader.replace(/_/g, ' ');
+		$('#skill-list').append(`<div class='${skillHeader}'></div>`);
+		$(`#skill-list .${skillHeader}`).append(
+			`<h3>${heading}</h3>
+				<span id='list-container-${skillHeader}' class='list-container'></span>`
+		);
+		for (var skill in skills[skillHeader]) {
+			let $skillSpan = $('<span>', { id: `${skills[skillHeader][skill]}`, class: 'skill' });
+			$skillSpan.text(skill);
+			$(`#list-container-${skillHeader}`).append($skillSpan);
+		}
+	}
+})();
+
+$('.skill').hover(
+	function (event) {
+		let attr = event.target.id.split('-');
+		$(this).css('color', attr[0]);
+		$(this).css('background-color', attr[1]);
+		$(this).css('box-shadow', `10px 10px 0 ${attr[2]}`);
+		$(this).css('border', `2px solid ${attr[2]}`);
+	},
+	function (event) {
+		let attr = event.target.id.split('-');
+		$(this).css('color', '#000');
+		$(this).css('background-color', '#fff');
+		$(this).css('box-shadow', ' 6px 6px 0 #c5c5c5');
+		$(this).css('border', `2px solid #000`);
+	}
+);
+
 //fade reveal
 ScrollReveal({
-    duration: 600,
-    reset: true,
-   easing: 'cubic-bezier(.694,0,.335,1)',
-   scale: 1,
-   viewFactor: 0.27,
+	duration: 600,
+	reset: true,
+	easing: 'cubic-bezier(.694,0,.335,1)',
+	scale: 1,
+	viewFactor: 0.1,
 }).reveal('.background');
 
 ScrollReveal({
-  duration: 600,
-  reset: true,
-  easing: 'cubic-bezier(.694,0,.335,1)',
-  scale: 1,
-  viewFactor: 0.27,
-}).reveal('.skills');
+	duration: 600,
+	reset: true,
+	easing: 'cubic-bezier(.694,0,.335,1)',
+	scale: 1,
+	viewFactor: 0.1,
+}).reveal('#skills');
 
-//Get the date
-(function (){
-  const date = new Date();
-  const year = date.getFullYear();
-  document.getElementById('year').innerText = year;
-})()
+ScrollReveal({
+	duration: 600,
+	reset: true,
+	easing: 'cubic-bezier(.694,0,.335,1)',
+	scale: 1,
+	viewFactor: 0.001,
+}).reveal('#projects');
